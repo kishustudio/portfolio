f20cc7689abfc4339aa4096a9d18bbaf0389378c
Kishu Kumar <kishukumar08521@gmail.com>
2026-04-05 19:50:33 +0530

    Updates

--- assets/Navbar-Be7NL5z3.js
+++ assets/Navbar-Be7NL5z3.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-KkPaEEbw.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-DOoZ0a8d.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
